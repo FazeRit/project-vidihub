6be9280e7d948a7cdf83afea7ef74b799ffbb025
denysHutsan <denys.hut@cgsteam.io>
2026-02-02 20:30:57 +0200

    feat: Added winston logger and exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatasourceModule } from './app/modules/datasource/datasource.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './app/configs/winston/winston.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    WinstonModule.forRoot(winstonConfig),
     DatasourceModule
   ],
   controllers: [],
