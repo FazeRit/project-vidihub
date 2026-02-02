68a6277610c2dff645236beff736456f402fc120
denysHutsan <denys.hut@cgsteam.io>
2026-02-02 20:42:42 +0200

    feat: Added throttler to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { DatasourceModule } from './app/modules/datasource/datasource.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './app/configs/winston/winston.config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { throttlerConfig } from './app/configs/throttler/throttler.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
-    WinstonModule.forRoot(winstonConfig),
-    DatasourceModule
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+          isGlobal: true
+        }),
+        WinstonModule.forRoot(winstonConfig),
+        ThrottlerModule.forRoot(throttlerConfig),
+        DatasourceModule
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
---
+++ src/app/configs/throttler/constants/throttler.const.ts
@@ -0,0 +1,11 @@
+export const THROTTLER_NAMES = {
+    SHORT: 'short',
+    MEDIUM: 'medium',
+    LONG: 'long',
+
+    AUTH_LOGIN: 'auth_login',
+    AUTH_REGISTER: 'auth_register',
+
+    VIDEO_UPLOAD: 'video_upload',
+    SEARCH: 'search'
+} as const;
