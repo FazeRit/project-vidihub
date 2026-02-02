96ba7a38ea1465edc25e13c16b1cce09d47b2ea3
denysHutsan <denys.hut@cgsteam.io>
2026-02-02 20:45:55 +0200

    feat: Added throttler guard for whole app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { DatasourceModule } from './app/modules/datasource/datasource.module';
 import { WinstonModule } from 'nest-winston';
 import { winstonConfig } from './app/configs/winston/winston.config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { throttlerConfig } from './app/configs/throttler/throttler.config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
     imports: [
@@ -15,7 +16,11 @@ import { throttlerConfig } from './app/configs/throttler/throttler.config';
         ThrottlerModule.forRoot(throttlerConfig),
         DatasourceModule
     ],
-    controllers: [],
-    providers: [],
+    providers: [
+      {
+        provide: APP_GUARD,
+        useClass: ThrottlerGuard
+      }
+    ],
 })
 export class AppModule {}
